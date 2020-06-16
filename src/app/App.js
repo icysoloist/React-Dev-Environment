dc0b2b6b33b126e9135405ed422a0a38bb9da290
Chase Pareti <chasepareti@gmail.com>
2020-06-16 18:56:33 -0500

    Changed tabs to spaces

--- src/app/App.js
+++ src/app/App.js
@@ -4,13 +4,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>It Works! </h1>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>It Works! </h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
